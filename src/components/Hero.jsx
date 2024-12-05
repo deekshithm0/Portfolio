ebfccd6a89a6c7d1e2329e539a17a4616a23d2ff
deekshithm0 <deekshithm0@gmail.com>
2024-12-05 21:27:09 +0530

    hero added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,39 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section id='home' className=''>
-        <div className=''>
+    <section id='home' className='pt-28 lg:pt-36'>
+        <div className='container items-center lg:grid lg:grid-cols-2 lg:gap-10'>
 
-            <div>
-                
+            <div>               
+              <div className='flex items-center gap-3'>
+                <figure className='img-box w-9 h-9 rounded-lg'>
+                  <img src="/images/avatar-1.jpg" width={40} height={40} alt="" className='img-cover' />
+                </figure>
+
+                 <div className='flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide'>
+                  <span className='relative w-2 h-2 rounded-full bg-emerald-400'>
+                    <span className='absolute inset-0 rounded-full bg-emerald-400 animate-ping'></span>
+                  </span>
+
+                  Available for Work
+                 </div>
+              </div>
+
+              <h2 className="headline-1">
+                Building Scalable Modern Websites for the Future
+              </h2>
+
+              <div className="">
+                ButtonPrimary
+
+                ButtonOutLine
+              </div>
+            </div>
+
+            <div className="">
+              <figure className="">
+                <img src="/images/hero-banner.png" width={656} height={600} alt="" className="w-full" />
+              </figure>
             </div>
 
         </div>
