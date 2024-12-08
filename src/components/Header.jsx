cbb452231a248514e5d7f6ddcd30197ec5d279cf
deekshithm0 <deekshithm0@gmail.com>
2024-12-08 13:31:13 +0530

    Profile updations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,8 @@ const Header = () => {
             <div className='max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]'>
                 <h1>
                     <a href="/" className='logo'>
-                        <img src="/images/logo.svg" width={40} height={40} alt="logo" />
+                        {/* <img src="/images/logo.svg" width={40} height={40} alt="logo" /> */}
+                        <h2 className='headline-2'>DEEKSHITH</h2>
                     </a>
                 </h1>
 
