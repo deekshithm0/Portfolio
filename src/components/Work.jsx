27bed6ea7b45b989925a852307470a003fb365ff
deekshithm0 <deekshithm0@gmail.com>
2025-03-31 13:55:05 +0530

    work update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,36 +5,37 @@ const works = [
   {
     imgSrc: '/images/almango.jpg',
     title: 'Al-Mango Food Menu App',
-    tags: ['React','Router', 'Hooks'],
+    tags: ['React', 'Router', 'Hooks'],
     projectLink: 'https://almango.netlify.app/'
   },
-  {
-    imgSrc: '/images/project-2.jpg',
-    title: 'Project 2',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://projectlink.com'
-  },
-  {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Project 3',
-    tags: ['API', 'MVC'],
-    projectLink: 'https://projectlink.com'
-  },
-  {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Project 4',
-    tags: ['API', 'MVC'],
-    projectLink: 'https://projectlink.com'
-  }
+  // {
+  //   imgSrc: '/images/project-2.jpg',
+  //   title: 'Project 2',
+  //   tags: ['API', 'SPA'],
+  //   projectLink: 'https://projectlink.com'
+  // },
+  // {
+  //   imgSrc: '/images/project-3.jpg',
+  //   title: 'Project 3',
+  //   tags: ['API', 'MVC'],
+  //   projectLink: 'https://projectlink.com'
+  // },
+  // {
+  //   imgSrc: '/images/project-4.jpg',
+  //   title: 'Project 4',
+  //   tags: ['API', 'MVC'],
+  //   projectLink: 'https://projectlink.com'
+  // }
 ]
 
 const Work = () => {
   return (
     <section id='work' className='section'>
       <div className='container flex flex-col items-center justify-center'>
-        <h2 className='headline-2 mb-8 text-center reveal-up'>My Portfolio Highlights</h2>
-
-        <div className='grid gap-x-4 gap-y-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 '>
+        <h2 className='headline-2 mb-8 text-center reveal-up'>
+          My Portfolio Highlights
+        </h2>
+        <div className='grid gap-x-4 gap-y-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {works.map(({ imgSrc, title, tags, projectLink }, key) => (
             <ProjectCard imgSrc={imgSrc} title={title} tags={tags} projectLink={projectLink} key={key} classes='reveal-up'/>
           ))}
